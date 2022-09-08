51fc6f89579b22b87ca4b963e50b32c864288630
aamircodes <aamirp1@outlook.com>
2022-09-09 00:04:18 +0100

    Initial commit - Add frontend

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import './App.css';
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    selectedFile: null,
+    fileUploadedSuccessfully: false,
+  };
+
+  onFileChange = (event) => {
+    this.setState({ selectedFile: event.target.files[0] });
+  };
+
+  onFileUpload = () => {
+    const formData = new FormData();
+    formData.append('demofile', this.state.selectedFile, this.state.selectedFile.name);
+
+    console.log(formData);
+    this.setState({ selectedFile: null });
+    this.setState({ fileUploadedSuccessfully: true });
+  };
+
+  fileData = () => {
+    if (this.state.selectedFile) {
+      return (
+        <div>
+          <h2>File Details: </h2>
+          <p>File Name: {this.state.selectedFile.name}</p>
+          <p>File Type: {this.state.selectedFile.type}</p>
+          <p>Last Modified: {this.state.selectedFile.lastModifiedDate.toDateString()} </p>
+        </div>
+      );
+    } else if (this.state.fileUploadedSuccessfully) {
+      return (
+        <div>
+          <br />
+          <h4>Your file has uploaded successfully</h4>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <br />
+          <h4>Choose a file and then press the Upload button</h4>
+        </div>
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div className='container'>
+        <h2>Meister File Upload System</h2>
+        <h3>File Upload with React and a Serverless API!</h3>
+        <div>
+          <input type='file' onChange={this.onFileChange} />
+          <button onClick={this.onFileUpload}>Upload</button>
+        </div>
+        {this.fileData()}
+      </div>
+    );
+  }
+}
+
+export default App;
